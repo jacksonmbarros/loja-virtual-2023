301d319b965c17f793b0bd7345edb675908c6c1a
Jackson <jmbarros270@gmail.com>
2023-01-30 23:47:12 -0300

    Envio de email

--- backend/src/main/java/com/dev/backend/service/PessoaClienteService.java
+++ backend/src/main/java/com/dev/backend/service/PessoaClienteService.java
@@ -3,7 +3,6 @@ package com.dev.backend.service;
 import com.dev.backend.dto.PessoaClienteRequestDTO;
 import com.dev.backend.entity.Pessoa;
 import com.dev.backend.repository.PessoaClienteRepository;
-import com.dev.backend.repository.PessoaRepository;
 import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,11 +16,15 @@ public class PessoaClienteService {
     @Autowired
     private PermissaoPessoaService permissaoPessoaService;
     
+    @Autowired
+    private EmailService emailService;
+    
     public Pessoa registrar(PessoaClienteRequestDTO pessoaClienteRequestDTO) {
         Pessoa pessoa = new PessoaClienteRequestDTO().converter(pessoaClienteRequestDTO);
         pessoa.setDataCriacao(new Date());
         Pessoa objetoNovo = pessoaClienteRepository.saveAndFlush(pessoa);
         permissaoPessoaService.vincularPessoaPermissaoCliente(objetoNovo);
+        emailService.enviarEmailTexto(objetoNovo.getEmail(), "Cadastro na Loja Virtual", "O registro na Loja Virtual foi realizado com sucesso. Em breve você receberá a senha de acesso por e-mail!!");
         return objetoNovo;
     }
     
